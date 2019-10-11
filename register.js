cd1d47ad2d5509a860f0da3a21a31e4c5948857e
unknown <mely.rrz@gmail.com>
2019-10-10 19:15:29 -0500

    Clase-22

---
+++ register.js
@@ -0,0 +1,36 @@
+
+const registerButton = document.getElementById('register');
+
+registerButton.addEventListener('click', printValues);
+
+window.addEventListener('load', getUsers);
+
+function getUsers(){
+    const headers = new Headers({'Content-Type': 'aplication/json'});
+    return fetch('http://localhost:3000/users',{
+        method: 'post',
+        headers: headers
+    }).then(response => {
+        return response.json();
+    }).then(res => console.log (res));
+}
+
+async function getAsyncUsers(){
+    const headers = new Headers({'Content-Type': 'application/json'});
+    const APIcall = await fetch('http://localhost:3000/users', {
+        method: 'post',
+        headers: headers});
+    const parseJSON = await APIcall.json();
+    const data = await parseJSON;
+    console.log(data);
+}
+
+function printValues(event){
+    event.preventDefault();
+    const data ={
+        name: document.getElementById('name').value,
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value, 
+    };
+    console.log(data);
+};
